28816afbe0e7eeb27a36879051b75138c0dade81
Gerardo Gómez-Caminero Ortega <gerardo.gomez@factorlibre.com>
2021-09-02 13:20:06 +0200

    [11.0][ADD] account_menu_invoice_refund: Backport from 12.0

--- account_menu_invoice_refund/__init__.py
+++ account_menu_invoice_refund/__init__.py
@@ -1,2 +1,3 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# © 2021 FactorLibre - Gerardo Gomez-Caminero <gerardo.gomez@factorlibre.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
