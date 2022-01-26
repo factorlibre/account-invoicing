bee8b183281793fbcdab1018e9f68149f650f05d
Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
2022-01-26 13:38:40 +0100

    [REF] account_invoice_supplier_ref_unique: remove leading and trailing spaces from supplier invoice number

--- account_invoice_supplier_ref_unique/__manifest__.py
+++ account_invoice_supplier_ref_unique/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2016 Acsone
+# © 2022 FactorLibre - Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Unique Supplier Invoice Number in Invoice',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'summary': 'Checks that supplier invoices are not entered twice',
     'author':
         "Savoir-faire Linux, Acsone SA/NV, Odoo Community Association (OCA)",
