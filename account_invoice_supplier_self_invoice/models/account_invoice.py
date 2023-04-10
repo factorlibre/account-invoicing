122f6ddd5a5e7cb5a1657a19250d9d576ebdb69e
Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
2023-04-10 16:31:45 +0200

    [FIX] account_invoice_supplier_self_invoice: self invoice number on copy Form view self_invoice_number visibility

--- account_invoice_supplier_self_invoice/models/account_invoice.py
+++ account_invoice_supplier_self_invoice/models/account_invoice.py
@@ -9,9 +9,9 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     self_invoice_number = fields.Char(
-        readonly=True
+        readonly=True, copy=False
     )
-    set_self_invoice = fields.Boolean(string='Set self invoice')
+    set_self_invoice = fields.Boolean(string='Set self invoice', copy=False)
     can_self_invoice = fields.Boolean(related='partner_id.self_invoice')
 
     @api.onchange('partner_id', 'company_id')
